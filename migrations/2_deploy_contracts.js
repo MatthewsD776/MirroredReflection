d57114e05c3d66f3b259a20430467ab07f26e7cd
Darren Matthews <darrenmatthews776@googlemail.com>
2019-08-03 10:40:25 +0100

    Made changes to make the test run

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,5 @@
-const SimpleStorage = artifacts.require("SimpleStorage");
-const TutorialToken = artifacts.require("TutorialToken");
-const ComplexStorage = artifacts.require("ComplexStorage");
 const SuggestionBoard = artifacts.require("SuggestionBoard");
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
-  deployer.deploy(TutorialToken);
-  deployer.deploy(ComplexStorage);
   deployer.deploy(SuggestionBoard);
 };
