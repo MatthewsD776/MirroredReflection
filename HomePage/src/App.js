2a907d56afa7079ad58ab592f6fc56dedc4a8b56
Darren Matthews <darrenmatthews776@googlemail.com>
2019-08-08 20:19:21 +0100

    Updated the Home page with pictures

--- HomePage/src/App.js
+++ HomePage/src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './App.css';
+import SingleLineGridList from './Components/SingleLineImages';
+import Arrow from "@material-ui/icons/ArrowDownward";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          Coming Soon to a Screen near you!
+          <h2>Coming Soon to a Screen near you!</h2>
+          <p>Enjoy some cool pictures!</p>
+          <Arrow/>
         </header>
+        <div className="App-intro">
+          <SingleLineGridList/>
+        </div>
       </div>
     );
   }
---
+++ HomePage/src/Data/tileData.jsx
@@ -0,0 +1,71 @@
+import image1 from '../Resources/image1.jpg';
+import image2 from '../Resources/image2.jpg';
+import image3 from '../Resources/image3.jpg';
+import image4 from '../Resources/image4.jpg';
+import image5 from '../Resources/image5.jpg';
+import image6 from '../Resources/image6.jpg';
+import image7 from '../Resources/image7.jpg';
+import image8 from '../Resources/image8.jpg';
+import image9 from '../Resources/image9.jpg';
+import image10 from '../Resources/image10.jpg';
+import image11 from '../Resources/image11.jpg';
+
+const tileData = [
+    {
+        img: image1,
+        title: 'Snow',
+        author: 'Anon',
+    },
+    {
+        img: image2,
+        title: 'Corridor',
+        author: 'Anon'
+    },
+    {
+        img: image3,
+        title: 'Circle',
+        author: 'Anon'
+    },
+    {
+        img: image4,
+        title: 'Dogs',
+        author: 'Anon'
+    },
+    {
+        img: image5,
+        title: 'Cups',
+        author: 'Anon'
+    },
+    {
+        img: image6,
+        title: 'Bottles',
+        author: 'Anon'
+    },
+    {
+        img: image7,
+        title: 'Up High',
+        author: 'Anon'
+    },
+    {
+        img: image8,
+        title: 'Rocks',
+        author: 'Anon'
+    },
+    {
+        img: image9,
+        title: 'Waterfall',
+        author: 'Anon'
+    },
+    {
+        img: image10,
+        title: 'Waves',
+        author: 'Anon'
+    },
+    {
+        img: image11,
+        title: 'Smokey Woman',
+        author: 'Anon'
+    }
+];
+ 
+export default tileData;
